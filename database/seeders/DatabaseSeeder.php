85e88ebc241365c498a196e094288623e9c3a02b
Josip Peroš <peros.josip@gmail.com>
2024-03-17 16:06:02 +0100

    added db seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,21 +2,44 @@
 
 namespace Database\Seeders;
 
-// use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use App\Enums\Roles;
+use App\Models\Role;
+use App\Models\User;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
 
 class DatabaseSeeder extends Seeder
 {
     /**
-     * Seed the application's database.
+     * Run the database seeds.
      */
     public function run(): void
     {
-        // \App\Models\User::factory(10)->create();
+        DB::beginTransaction();
 
-        // \App\Models\User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
+        try {
+            $admin = new Role();
+            $admin->name = Roles::ADMIN;
+            $admin->save();
+
+            $moderator = new Role();
+            $moderator->name = Roles::MODERATOR;
+            $moderator->save();
+
+            $superUser = new User();
+
+            $superUser->name = 'SuperUser';
+            $superUser->email = 'superuser@email.com';
+            $superUser->password = '$2y$12$60cRCCCDG1.3SbMR2mmRje9gSmTBLnANtLhJigauwzRg3zn1MMh4a';
+            $superUser->save();
+
+            $superUser->roles()->attach($admin->id);
+        } catch (\Throwable $th) {
+            DB::rollBack();
+            throw $th;
+        }
+
+        DB::commit();
     }
 }
