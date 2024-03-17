d0cce877431e793b4d54809fb220f7be049c2628
Josip Peroš <peros.josip@gmail.com>
2024-03-17 16:05:43 +0100

    added admin and moderator gates

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,7 +2,12 @@
 
 namespace App\Providers;
 
+use App\Enums\Roles;
+use App\Models\Role;
+use App\Models\User;
+use Illuminate\Support\Facades\Gate;
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Database\Eloquent\Builder;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -19,6 +24,26 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        // Should return TRUE or FALSE
+        Gate::define('admin', function (User $user) {
+            $admin = Role::where('name', Roles::ADMIN)->first();
+
+            $user = User::where('users.id', $user->id)->whereRelation('roles', 'roles.id', $admin->id)->first();
+
+            if ($user) {
+                return true;
+            }
+        });
+
+        // Should return TRUE or FALSE
+        Gate::define('moderator', function (User $user) {
+            $moderator = Role::where('name', Roles::MODERATOR)->first();
+
+            $user = User::where('users.id', $user->id)->whereRelation('roles', 'roles.id', $moderator->id)->first();
+
+            if ($user) {
+                return true;
+            }
+        });
     }
 }
